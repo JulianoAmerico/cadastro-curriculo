7361f8029081f758e6b374612091f4891f1c4010
Juliano Americo <juriam_man@hotmail.com>
2019-01-10 18:37:33 -0200

    Classe como abstract e criação da strategy.

--- src/main/java/br/com/juliano/javaee/model/Cursos.java
+++ src/main/java/br/com/juliano/javaee/model/Cursos.java
@@ -6,12 +6,15 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @MappedSuperclass
-public class Cursos implements Serializable {
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+public abstract class Cursos implements Serializable {
 
 	@Id
 	@GeneratedValue
