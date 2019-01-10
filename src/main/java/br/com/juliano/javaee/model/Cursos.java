323af8217389ecc090b51512056ea77230ab5c37
Juliano Americo <juriam_man@hotmail.com>
2019-01-10 18:35:01 -0200

    Criação da superclasse Cursos.

---
+++ src/main/java/br/com/juliano/javaee/model/Cursos.java
@@ -0,0 +1,65 @@
+package br.com.juliano.javaee.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@MappedSuperclass
+public class Cursos implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@Column(nullable = false, length = 70)
+	private String curso;
+
+	@Column(nullable = false, length = 70)
+	private String instituicao;
+
+	@Temporal(TemporalType.DATE)
+	@Column(nullable = true, length = 10)
+	private Date dataConclusao;
+
+	public Cursos() {
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCurso() {
+		return this.curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public String getInstituicao() {
+		return this.instituicao;
+	}
+
+	public void setInstituicao(String instituicao) {
+		this.instituicao = instituicao;
+	}
+
+	public Date getDataConclusao() {
+		return this.dataConclusao;
+	}
+
+	public void setDataConclusao(Date dataConclusao) {
+		this.dataConclusao = dataConclusao;
+	}
+
+}
