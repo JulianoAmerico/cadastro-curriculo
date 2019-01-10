c8225415c30f6188b30c57574d5e227c8f9a85ee
Juliano Americo <juriam_man@hotmail.com>
2019-01-10 18:43:52 -0200

    Criação da entidade Cursos Complementares.

---
+++ src/main/java/br/com/juliano/javaee/model/CursosComplementares.java
@@ -0,0 +1,12 @@
+package br.com.juliano.javaee.model;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class CursosComplementares extends Cursos {
+
+	public CursosComplementares() {
+	}
+
+}
