ac1188244022d042f90e62f0cadcdc55995ce2c2
Juliano Americo <juriam_man@hotmail.com>
2019-01-10 16:54:34 -0200

    First commit

---
+++ src/main/java/br/com/juliano/config/Config.java
@@ -0,0 +1,15 @@
+package br.com.juliano.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
+
+/*
+ * CDI Beans: Gerencia o ciclo de vida de alguns objetos dentro da app java ee.
+ */
+
+@FacesConfig(version = Version.JSF_2_3)
+@ApplicationScoped
+public class Config {
+	
+}
