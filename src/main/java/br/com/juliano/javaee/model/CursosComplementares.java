00979830a001d5eb06f2ad289cdd627c075e9071
Juliano Americo <juriam_man@hotmail.com>
2019-01-10 21:23:17 -0200

    Criado o relacionamento ao candidato.

--- src/main/java/br/com/juliano/javaee/model/CursosComplementares.java
+++ src/main/java/br/com/juliano/javaee/model/CursosComplementares.java
@@ -1,13 +1,24 @@
 package br.com.juliano.javaee.model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="cursos_complementares")
+@Table(name = "cursos_complementares")
 public class CursosComplementares extends Cursos {
+
+	@ManyToOne(name = "candidato_id")
+	private Candidato candidato;
 
 	public CursosComplementares() {
 	}
 
+	public Candidato getCandidato() {
+		return candidato;
+	}
+
+	public void setCandidato(Candidato candidato) {
+		this.candidato = candidato;
+	}
 }
