696cbad159ece4428a1eed96e628501a3a94a9c3
Juliano Americo <juriam_man@hotmail.com>
2019-01-11 13:11:15 -0200

    Criação de um validador de data de nascimento.

---
+++ src/main/java/br/com/juliano/javaee/validator/DateValidator.java
@@ -0,0 +1,29 @@
+package br.com.juliano.javaee.validator;
+
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+public class DateValidator implements Validator<Date>{
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Date date) throws ValidatorException {
+		boolean result = date == null ? true : date.toString().trim().isEmpty();
+		FacesMessage message;
+		if(result) {
+			message = new FacesMessage("Data é inválida");
+			throw new ValidatorException(message);
+		}
+		
+		Date now = new Date();
+		if(date.after(now)) {
+			message = new FacesMessage("A data digitada é maior que a data atual");
+			throw new ValidatorException(message);
+		}
+	}
+
+}
