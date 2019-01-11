351367e69d3c1bf243dc69eacaea51213f685f1b
Juliano Americo <juriam_man@hotmail.com>
2019-01-11 13:20:35 -0200

    Atualização do método.

--- src/main/java/br/com/juliano/javaee/validator/DateBirthValidator.java
+++ src/main/java/br/com/juliano/javaee/validator/DateBirthValidator.java
@@ -5,9 +5,11 @@ import java.util.Date;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
+@FacesValidator("dateBirthValidator")
 public class DateBirthValidator implements Validator<Date>{
 
 	@Override
@@ -25,5 +27,4 @@ public class DateBirthValidator implements Validator<Date>{
 			throw new ValidatorException(message);
 		}
 	}
-
 }
