c7a4a0e5a3cf636c7706321372c64c361dc77a95
Juliano Americo <juriam_man@hotmail.com>
2019-01-11 13:03:11 -0200

    Atualização de métodos.

--- src/main/java/br/com/juliano/javaee/ejb/CandidatoBean.java
+++ src/main/java/br/com/juliano/javaee/ejb/CandidatoBean.java
@@ -1,16 +1,30 @@
 package br.com.juliano.javaee.ejb;
 
-import javax.ejb.Stateful;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@Stateful
+import br.com.juliano.javaee.model.Candidato;
+
+/**
+ * Session Bean que faz o CRUD do Candidato.
+ * @author Juliano R. Américo
+ *
+ */
+@Stateless
 public class CandidatoBean {
 
 	@PersistenceContext
 	private EntityManager em;
 	
     public CandidatoBean() {
-       
+    }
+    
+    /**
+     * Grava os do candidato no banco de dados, fornecido pelo formulário do usuário.
+     * @param candidato Dados do candidato preenchido.
+     */
+    public void gravar(Candidato candidato) {
+    	em.persist(candidato);
     }
 }
