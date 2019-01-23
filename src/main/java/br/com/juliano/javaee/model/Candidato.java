350c4ff53cb8c8c0b2e266f2f75095de1d5bbc71
Juliano Americo <juriam_man@hotmail.com>
2019-01-23 20:08:22 -0200

    Inclusão do atributo dataCadastro.

--- src/main/java/br/com/juliano/javaee/model/Candidato.java
+++ src/main/java/br/com/juliano/javaee/model/Candidato.java
@@ -20,219 +20,228 @@ import javax.persistence.TemporalType;
 @Table(name = "candidato")
 public class Candidato implements Serializable {
 
-	public enum Sexo {
-		FEMININO("Feminino"), MASCULINO("Masculino");
+    public enum Sexo {
+        FEMININO("Feminino"), MASCULINO("Masculino");
 
-		private String label;
+        private String label;
 
-		Sexo(String label) {
-			this.label = label;
-		}
+        Sexo(String label) {
+            this.label = label;
+        }
 
-		public String getLabel() {
-			return label;
-		}
-	}
+        public String getLabel() {
+            return label;
+        }
+    }
 
-	public enum EstadoCivil {
-		SOLTEIRO("Solteiro(a)"), 
-		CASADO("Casado(a)"), 
-		VIUVO("Viuvo(a)"), 
-		DIVORCIADO("Divorciado(a)");
+    public enum EstadoCivil {
+        SOLTEIRO("Solteiro(a)"), CASADO("Casado(a)"), VIUVO("Viuvo(a)"), DIVORCIADO("Divorciado(a)");
 
-		private String label;
+        private String label;
 
-		private EstadoCivil(String label) {
-			this.label = label;
-		}
+        private EstadoCivil(String label) {
+            this.label = label;
+        }
 
-		public String getLabel() {
-			return label;
-		}
+        public String getLabel() {
+            return label;
+        }
 
-	}
+    }
 
-	@Id
-	@GeneratedValue
-	private Integer id;
+    @Id
+    @GeneratedValue
+    private Integer id;
 
-	@Column(nullable = false, length = 50)
-	private String nome;
+    @Column(nullable = false, length = 50)
+    private String nome;
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private Sexo sexo;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Sexo sexo;
 
-	@Embedded
-	private Endereco endereco;
+    @Embedded
+    private Endereco endereco;
 
-	@Column(nullable = false, length = 30)
-	private String nacionalidade;
+    @Column(nullable = false, length = 30)
+    private String nacionalidade;
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private EstadoCivil estadoCivil;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EstadoCivil estadoCivil;
 
-	@Temporal(TemporalType.DATE)
-	@Column(nullable = false, length = 10)
-	private Date dataNascimento;
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false, length = 10)
+    private Date dataNascimento;
 
-	@Column(nullable = false, length = 14)
-	private String telefoneCelular;
+    @Column(nullable = false, length = 14)
+    private String telefoneCelular;
 
-	@Column(nullable = false, length = 50)
-	private String email;
+    @Column(nullable = false, length = 50)
+    private String email;
 
-	@Column(nullable = true, length = 100)
-	private String linkedin;
+    @Column(nullable = true, length = 100)
+    private String linkedin;
 
-	@Column(nullable = false, length = 50)
-	private String objetivoProfissional;
+    @Column(nullable = false, length = 50)
+    private String objetivoProfissional;
 
-	@Column(nullable = false)
-	private Double pretensaoSalarial;
+    @Column(nullable = false)
+    private Double pretensaoSalarial;
 
-	@OneToMany(mappedBy = "candidato")
-	private List<ExperienciaProfissional> expProfissional;
+    @OneToMany(mappedBy = "candidato")
+    private List<ExperienciaProfissional> expProfissional;
 
-	@OneToMany(mappedBy = "candidato")
-	private List<Idioma> idiomas;
+    @OneToMany(mappedBy = "candidato")
+    private List<Idioma> idiomas;
 
-	@OneToMany(mappedBy = "candidato")
-	private List<FormacaoAcademica> formacaoAcademica;
+    @OneToMany(mappedBy = "candidato")
+    private List<FormacaoAcademica> formacaoAcademica;
 
-	@OneToMany(mappedBy = "candidato")
-	private List<CursosComplementares> cursosComplementares;
+    @OneToMany(mappedBy = "candidato")
+    private List<CursosComplementares> cursosComplementares;
 
-	public Candidato() {
-	}
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false)
+    private Date dataCadastro;
 
-	public Integer getId() {
-		return this.id;
-	}
+    public Candidato() {
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Integer getId() {
+        return this.id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public Sexo getSexo() {
-		return sexo;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setSexo(Sexo sexo) {
-		this.sexo = sexo;
-	}
+    public Sexo getSexo() {
+        return sexo;
+    }
 
-	public Endereco getEndereco() {
-		return this.endereco;
-	}
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
 
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
+    public Endereco getEndereco() {
+        return this.endereco;
+    }
 
-	public String getNacionalidade() {
-		return this.nacionalidade;
-	}
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 
-	public void setNacionalidade(String nacionalidade) {
-		this.nacionalidade = nacionalidade;
-	}
+    public String getNacionalidade() {
+        return this.nacionalidade;
+    }
 
-	public EstadoCivil getEstadoCivil() {
-		return this.estadoCivil;
-	}
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
 
-	public void setEstadoCivil(EstadoCivil estadoCivil) {
-		this.estadoCivil = estadoCivil;
-	}
+    public EstadoCivil getEstadoCivil() {
+        return this.estadoCivil;
+    }
 
-	public Date getDataNascimento() {
-		return this.dataNascimento;
-	}
+    public void setEstadoCivil(EstadoCivil estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
 
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
+    public Date getDataNascimento() {
+        return this.dataNascimento;
+    }
 
-	public String getTelefoneCelular() {
-		return this.telefoneCelular;
-	}
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 
-	public void setTelefoneCelular(String telefoneCelular) {
-		this.telefoneCelular = telefoneCelular;
-	}
+    public String getTelefoneCelular() {
+        return this.telefoneCelular;
+    }
 
-	public String getEmail() {
-		return this.email;
-	}
+    public void setTelefoneCelular(String telefoneCelular) {
+        this.telefoneCelular = telefoneCelular;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return this.email;
+    }
 
-	public String getLinkedin() {
-		return this.linkedin;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setLinkedin(String linkedin) {
-		this.linkedin = linkedin;
-	}
+    public String getLinkedin() {
+        return this.linkedin;
+    }
 
-	public String getObjetivoProfissional() {
-		return this.objetivoProfissional;
-	}
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
 
-	public void setObjetivoProfissional(String objetivoProfissional) {
-		this.objetivoProfissional = objetivoProfissional;
-	}
+    public String getObjetivoProfissional() {
+        return this.objetivoProfissional;
+    }
 
-	public Double getPretensaoSalarial() {
-		return this.pretensaoSalarial;
-	}
+    public void setObjetivoProfissional(String objetivoProfissional) {
+        this.objetivoProfissional = objetivoProfissional;
+    }
 
-	public void setPretensaoSalarial(Double pretensaoSalarial) {
-		this.pretensaoSalarial = pretensaoSalarial;
-	}
+    public Double getPretensaoSalarial() {
+        return this.pretensaoSalarial;
+    }
 
-	public List<ExperienciaProfissional> getExpProfissional() {
-		return expProfissional;
-	}
+    public void setPretensaoSalarial(Double pretensaoSalarial) {
+        this.pretensaoSalarial = pretensaoSalarial;
+    }
 
-	public void setExpProfissional(List<ExperienciaProfissional> expProfissional) {
-		this.expProfissional = expProfissional;
-	}
+    public List<ExperienciaProfissional> getExpProfissional() {
+        return expProfissional;
+    }
 
-	public List<Idioma> getIdiomas() {
-		return idiomas;
-	}
+    public void setExpProfissional(List<ExperienciaProfissional> expProfissional) {
+        this.expProfissional = expProfissional;
+    }
 
-	public void setIdiomas(List<Idioma> idiomas) {
-		this.idiomas = idiomas;
-	}
+    public List<Idioma> getIdiomas() {
+        return idiomas;
+    }
 
-	public List<FormacaoAcademica> getFormacaoAcademica() {
-		return formacaoAcademica;
-	}
+    public void setIdiomas(List<Idioma> idiomas) {
+        this.idiomas = idiomas;
+    }
 
-	public void setFormacaoAcademica(List<FormacaoAcademica> formacaoAcademica) {
-		this.formacaoAcademica = formacaoAcademica;
-	}
+    public List<FormacaoAcademica> getFormacaoAcademica() {
+        return formacaoAcademica;
+    }
 
-	public List<CursosComplementares> getCursosComplementares() {
-		return cursosComplementares;
-	}
+    public void setFormacaoAcademica(List<FormacaoAcademica> formacaoAcademica) {
+        this.formacaoAcademica = formacaoAcademica;
+    }
 
-	public void setCursosComplementares(List<CursosComplementares> cursosComplementares) {
-		this.cursosComplementares = cursosComplementares;
-	}
+    public List<CursosComplementares> getCursosComplementares() {
+        return cursosComplementares;
+    }
+
+    public void setCursosComplementares(List<CursosComplementares> cursosComplementares) {
+        this.cursosComplementares = cursosComplementares;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
 
 }
