3e1390a33623040d271c85aa33699a6906d03389
Juliano Americo <juriam_man@hotmail.com>
2019-01-24 18:46:33 -0200

    Criação do método consultarPorData;

--- src/main/java/br/com/juliano/javaee/ejb/CandidatoBean.java
+++ src/main/java/br/com/juliano/javaee/ejb/CandidatoBean.java
@@ -1,10 +1,12 @@
 package br.com.juliano.javaee.ejb;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import br.com.juliano.javaee.model.Candidato;
 import br.com.juliano.javaee.model.CursosComplementares;
@@ -14,7 +16,7 @@ import br.com.juliano.javaee.model.Idioma;
 
 /**
  * Session Bean que faz o CRUD do Candidato.
- * 
+ *
  * @author Juliano R. Américo
  *
  */
@@ -30,7 +32,7 @@ public class CandidatoBean {
 	/**
 	 * Grava os do candidato no banco de dados, fornecido pelo formulário do
 	 * usuário.
-	 * 
+	 *
 	 * @param candidato Dados do candidato preenchido.
 	 */
 	public void gravar(Candidato candidato, List<ExperienciaProfissional> expProfissional,
@@ -43,7 +45,7 @@ public class CandidatoBean {
 			e.setCandidato(candidato);
 			em.persist(e);
 		});
-		
+
 		formacaoAcademica.forEach(e -> {
 			e.setCandidato(candidato);
 			em.persist(e);
@@ -58,6 +60,12 @@ public class CandidatoBean {
 			e.setCandidato(candidato);
 			em.persist(e);
 		});
+	}
 
+	public List<Candidato> consultarPorData(Date consulta){
+	    String jpql = "SELECT c FROM Candidato c WHERE c.dataCadastro = :data ORDER BY c.dataCadastro DESC";
+        TypedQuery<Candidato> q = em.createQuery(jpql, Candidato.class);
+        q.setParameter("data", consulta);
+	    return q.getResultList();
 	}
 }
