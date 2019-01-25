b7a387e71a135da3a8858ce10dff55e4081a8bf6
Juliano Americo <juriam_man@hotmail.com>
2019-01-25 09:38:11 -0200

    Mudança da implementação do método consultarPorData.

--- src/main/java/br/com/juliano/javaee/ejb/CandidatoBean.java
+++ src/main/java/br/com/juliano/javaee/ejb/CandidatoBean.java
@@ -23,49 +23,59 @@ import br.com.juliano.javaee.model.Idioma;
 @Stateless
 public class CandidatoBean {
 
-	@PersistenceContext
-	private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-	public CandidatoBean() {
-	}
+    public CandidatoBean() {
+    }
 
-	/**
-	 * Grava os do candidato no banco de dados, fornecido pelo formulário do
-	 * usuário.
-	 *
-	 * @param candidato Dados do candidato preenchido.
-	 */
-	public void gravar(Candidato candidato, List<ExperienciaProfissional> expProfissional,
-			List<FormacaoAcademica> formacaoAcademica, List<CursosComplementares> cursosComplementares,
-			List<Idioma> idiomas) {
+    /**
+     * Grava os do candidato no banco de dados, fornecido pelo formulário do usuário.
+     *
+     * @param candidato Dados do candidato preenchido.
+     */
+    public void gravar(Candidato candidato, List<ExperienciaProfissional> expProfissional,
+            List<FormacaoAcademica> formacaoAcademica, List<CursosComplementares> cursosComplementares, List<Idioma> idiomas) {
 
-		em.persist(candidato);
+        em.persist(candidato);
 
-		expProfissional.forEach(e -> {
-			e.setCandidato(candidato);
-			em.persist(e);
-		});
+        expProfissional.forEach(e -> {
+            e.setCandidato(candidato);
+            em.persist(e);
+        });
 
-		formacaoAcademica.forEach(e -> {
-			e.setCandidato(candidato);
-			em.persist(e);
-		});
+        formacaoAcademica.forEach(e -> {
+            e.setCandidato(candidato);
+            em.persist(e);
+        });
 
-		cursosComplementares.forEach(e -> {
-			e.setCandidato(candidato);
-			em.persist(e);
-		});
+        cursosComplementares.forEach(e -> {
+            e.setCandidato(candidato);
+            em.persist(e);
+        });
 
-		idiomas.forEach(e -> {
-			e.setCandidato(candidato);
-			em.persist(e);
-		});
-	}
+        idiomas.forEach(e -> {
+            e.setCandidato(candidato);
+            em.persist(e);
+        });
+    }
 
-	public List<Candidato> consultarPorData(Date consulta){
-	    String jpql = "SELECT c FROM Candidato c WHERE c.dataCadastro = :data ORDER BY c.dataCadastro DESC";
-        TypedQuery<Candidato> q = em.createQuery(jpql, Candidato.class);
-        q.setParameter("data", consulta);
-	    return q.getResultList();
-	}
+    @SuppressWarnings("deprecation")
+    public List<Candidato> consultarPorData(Date consulta, Boolean tipoFiltro) {
+        if (tipoFiltro) {
+            String jpql = "SELECT c FROM Candidato c WHERE c.dataCadastro = :data ORDER BY c.dataCadastro DESC";
+            TypedQuery<Candidato> q = em.createQuery(jpql, Candidato.class);
+            q.setParameter("data", consulta);
+
+            return q.getResultList();
+        } else {
+            String jpql = "SELECT c FROM Candidato c WHERE DAY(c.dataCadastro) = :dia AND MONTH(c.dataCadastro) = :mes";
+            TypedQuery<Candidato> q = em.createQuery(jpql, Candidato.class);
+            q.setParameter("dia", consulta.getDay());
+            q.setParameter("mes", consulta.getMonth());
+
+            return q.getResultList();
+        }
+
+    }
 }
