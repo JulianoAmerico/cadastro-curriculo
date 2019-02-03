f7fe56069f3bb19ed4f345568decbffa0eae3bc6
Juliano Americo <juriam_man@hotmail.com>
2019-02-03 13:07:56 -0200

    Criado variável que limita a data de nascimento.

--- src/main/java/br/com/juliano/javaee/bean/FormularioBean.java
+++ src/main/java/br/com/juliano/javaee/bean/FormularioBean.java
@@ -38,188 +38,210 @@ import br.com.juliano.javaee.model.Idioma.Niveis;
 @ConversationScoped
 public class FormularioBean implements Serializable {
 
-	@EJB
-	private CandidatoBean candidatoBean;
+    @EJB
+    private CandidatoBean candidatoBean;
 
-	@Inject
-	private Conversation conversation;
+    @Inject
+    private Conversation conversation;
 
-	private Candidato candidato = new Candidato();
+    private Candidato candidato = new Candidato();
 
-	private Sexo[] sexoValues = Candidato.Sexo.values();
+    private Sexo[] sexoValues = Candidato.Sexo.values();
 
-	private EstadoCivil[] estadoCivilValues = Candidato.EstadoCivil.values();
+    private EstadoCivil[] estadoCivilValues = Candidato.EstadoCivil.values();
 
-	private NivelFormacao[] nivelFormacaoValues = FormacaoAcademica.NivelFormacao.values();
+    private NivelFormacao[] nivelFormacaoValues = FormacaoAcademica.NivelFormacao.values();
 
-	private Idiomas[] idiomasValues = Idioma.Idiomas.values();
+    private Idiomas[] idiomasValues = Idioma.Idiomas.values();
 
-	private Niveis[] niveisValues = Idioma.Niveis.values();
+    private Niveis[] niveisValues = Idioma.Niveis.values();
 
-	private Collection<String> estados;
+    private Collection<String> estados;
 
-	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-	/**
-	 * Cria os objetos necessários para guardar as informações do candidato.
-	 */
-	public FormularioBean() {
-		candidato.setEndereco(new Endereco());
-		candidato.setExpProfissional(new ArrayList<>());
-		candidato.setIdiomas(new ArrayList<>());
-		candidato.setCursosComplementares(new ArrayList<>());
-		candidato.setFormacaoAcademica(new ArrayList<>());
-		candidato.setSexo(Candidato.Sexo.FEMININO);
-	}
+    private Date minDate;
+    private Date maxDate;
 
-	@PostConstruct
-	public void init() {
-		conversation.begin();
-		estados = EstadosProvider.getEstadosCollection();
-	}
+    /**
+     * Cria os objetos necessários para guardar as informações do candidato.
+     */
+    public FormularioBean() {
+        candidato.setEndereco(new Endereco());
+        candidato.setExpProfissional(new ArrayList<>());
+        candidato.setIdiomas(new ArrayList<>());
+        candidato.setCursosComplementares(new ArrayList<>());
+        candidato.setFormacaoAcademica(new ArrayList<>());
+        candidato.setSexo(Candidato.Sexo.FEMININO);
+    }
 
-	public String processar() {
-	    String now = sdf.format(new Date());
+    @PostConstruct
+    public void init() {
+        conversation.begin();
+        estados = EstadosProvider.getEstadosCollection();
 
-	    try {
+        /* Limitando data de aniversário */
+        maxDate = new Date();
+    }
+
+    public String processar() {
+        String now = sdf.format(new Date());
+
+        try {
             candidato.setDataCadastro(sdf.parse(now));
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-		candidatoBean.gravar(candidato, candidato.getExpProfissional(), candidato.getFormacaoAcademica(),
-				candidato.getCursosComplementares(), candidato.getIdiomas());
+        candidatoBean.gravar(candidato, candidato.getExpProfissional(), candidato.getFormacaoAcademica(),
+                candidato.getCursosComplementares(), candidato.getIdiomas());
 
-		conversation.end();
-		return "sucesso?faces-redirect=true";
-	}
+        conversation.end();
+        return "sucesso?faces-redirect=true";
+    }
 
-	public String inserirExpProfissional() {
-		ExperienciaProfissional exp = new ExperienciaProfissional();
-		exp.setEdicao(true);
-		candidato.getExpProfissional().add(exp);
-		return null;
-	}
+    public String inserirExpProfissional() {
+        ExperienciaProfissional exp = new ExperienciaProfissional();
+        exp.setEdicao(true);
+        candidato.getExpProfissional().add(exp);
+        return null;
+    }
 
-	public String salvarExpProfissional(ExperienciaProfissional exp) {
-		exp.setEdicao(false);
-		return null;
-	}
+    public String salvarExpProfissional(ExperienciaProfissional exp) {
+        exp.setEdicao(false);
+        return null;
+    }
 
-	public String editarExpProfissional(ExperienciaProfissional exp) {
-		exp.setEdicao(true);
-		return null;
-	}
+    public String editarExpProfissional(ExperienciaProfissional exp) {
+        exp.setEdicao(true);
+        return null;
+    }
 
-	public String excluirExpProfissional(ExperienciaProfissional exp) {
-		candidato.getExpProfissional().remove(exp);
-		return null;
-	}
+    public String excluirExpProfissional(ExperienciaProfissional exp) {
+        candidato.getExpProfissional().remove(exp);
+        return null;
+    }
 
-	public String inserirFormacaoAcademica() {
-		FormacaoAcademica formacaoAcademica = new FormacaoAcademica();
-		formacaoAcademica.setEdicao(true);
-		candidato.getFormacaoAcademica().add(formacaoAcademica);
-		return null;
-	}
+    public String inserirFormacaoAcademica() {
+        FormacaoAcademica formacaoAcademica = new FormacaoAcademica();
+        formacaoAcademica.setEdicao(true);
+        candidato.getFormacaoAcademica().add(formacaoAcademica);
+        return null;
+    }
 
-	public String salvarFormacaoAcademica(FormacaoAcademica fa) {
-		fa.setEdicao(false);
-		return null;
-	}
+    public String salvarFormacaoAcademica(FormacaoAcademica fa) {
+        fa.setEdicao(false);
+        return null;
+    }
 
-	public String editarFormacaoAcademica(FormacaoAcademica fa) {
-		fa.setEdicao(true);
-		return null;
-	}
+    public String editarFormacaoAcademica(FormacaoAcademica fa) {
+        fa.setEdicao(true);
+        return null;
+    }
 
-	public String excluirFormacaoAcademica(FormacaoAcademica fa) {
-		candidato.getFormacaoAcademica().remove(fa);
-		return null;
-	}
+    public String excluirFormacaoAcademica(FormacaoAcademica fa) {
+        candidato.getFormacaoAcademica().remove(fa);
+        return null;
+    }
 
-	public String inserirCursosComplementares() {
-		CursosComplementares cursoComplementar = new CursosComplementares();
-		cursoComplementar.setEdicao(true);
-		candidato.getCursosComplementares().add(cursoComplementar);
-		return null;
-	}
+    public String inserirCursosComplementares() {
+        CursosComplementares cursoComplementar = new CursosComplementares();
+        cursoComplementar.setEdicao(true);
+        candidato.getCursosComplementares().add(cursoComplementar);
+        return null;
+    }
 
-	public String salvarCursosComplementares(CursosComplementares cursosComplementares) {
-		cursosComplementares.setEdicao(false);
-		return null;
-	}
+    public String salvarCursosComplementares(CursosComplementares cursosComplementares) {
+        cursosComplementares.setEdicao(false);
+        return null;
+    }
 
-	public String editarCursosComplementares(CursosComplementares cursosComplementares) {
-		cursosComplementares.setEdicao(true);
-		return null;
-	}
+    public String editarCursosComplementares(CursosComplementares cursosComplementares) {
+        cursosComplementares.setEdicao(true);
+        return null;
+    }
 
-	public String excluirCursosComplementares(CursosComplementares cursosComplementares) {
-		candidato.getCursosComplementares().remove(cursosComplementares);
-		return null;
-	}
+    public String excluirCursosComplementares(CursosComplementares cursosComplementares) {
+        candidato.getCursosComplementares().remove(cursosComplementares);
+        return null;
+    }
 
-	public String inserirIdioma() {
-		Idioma idioma = new Idioma();
-		idioma.setEdicao(true);
-		candidato.getIdiomas().add(idioma);
-		idioma.setCandidato(candidato);
-		return null;
-	}
+    public String inserirIdioma() {
+        Idioma idioma = new Idioma();
+        idioma.setEdicao(true);
+        candidato.getIdiomas().add(idioma);
+        idioma.setCandidato(candidato);
+        return null;
+    }
 
-	public String salvarIdiomas(Idioma idioma) {
-		idioma.setEdicao(false);
-		return null;
-	}
+    public String salvarIdiomas(Idioma idioma) {
+        idioma.setEdicao(false);
+        return null;
+    }
 
-	public String editarIdiomas(Idioma idioma) {
-		idioma.setEdicao(true);
-		return null;
-	}
+    public String editarIdiomas(Idioma idioma) {
+        idioma.setEdicao(true);
+        return null;
+    }
 
-	public String excluirIdiomas(Idioma idioma) {
-		candidato.getIdiomas().remove(idioma);
-		return null;
-	}
+    public String excluirIdiomas(Idioma idioma) {
+        candidato.getIdiomas().remove(idioma);
+        return null;
+    }
 
-	public CandidatoBean getCandidatoBean() {
-		return candidatoBean;
-	}
+    public CandidatoBean getCandidatoBean() {
+        return candidatoBean;
+    }
 
-	public void setCandidatoBean(CandidatoBean candidatoBean) {
-		this.candidatoBean = candidatoBean;
-	}
+    public void setCandidatoBean(CandidatoBean candidatoBean) {
+        this.candidatoBean = candidatoBean;
+    }
 
-	public Candidato getCandidato() {
-		return candidato;
-	}
+    public Candidato getCandidato() {
+        return candidato;
+    }
 
-	public void setCandidato(Candidato candidato) {
-		this.candidato = candidato;
-	}
+    public void setCandidato(Candidato candidato) {
+        this.candidato = candidato;
+    }
 
-	public Sexo[] getSexoValues() {
-		return sexoValues;
-	}
+    public Sexo[] getSexoValues() {
+        return sexoValues;
+    }
 
-	public EstadoCivil[] getEstadoCivilValues() {
-		return estadoCivilValues;
-	}
+    public EstadoCivil[] getEstadoCivilValues() {
+        return estadoCivilValues;
+    }
 
-	public NivelFormacao[] getNivelFormacaoValues() {
-		return nivelFormacaoValues;
-	}
+    public NivelFormacao[] getNivelFormacaoValues() {
+        return nivelFormacaoValues;
+    }
 
-	public Idiomas[] getIdiomasValues() {
-		return idiomasValues;
-	}
+    public Idiomas[] getIdiomasValues() {
+        return idiomasValues;
+    }
 
-	public Niveis[] getNiveisValues() {
-		return niveisValues;
-	}
+    public Niveis[] getNiveisValues() {
+        return niveisValues;
+    }
 
-	public Collection<String> getEstados() {
-		return estados;
-	}
+    public Collection<String> getEstados() {
+        return estados;
+    }
+
+    public Date getMinDate() {
+        return minDate;
+    }
+
+    public void setMinDate(Date minDate) {
+        this.minDate = minDate;
+    }
+
+    public Date getMaxDate() {
+        return maxDate;
+    }
+
+    public void setMaxDate(Date maxDate) {
+        this.maxDate = maxDate;
+    }
 }
